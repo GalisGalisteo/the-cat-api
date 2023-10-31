68c7eec65ad91580d718f74ef938ca1ca0cbc60c
GalisGalisteo <galis.travelling@gmail.com>
2023-10-31 12:18:50 +0100

    finished with bonus and extra

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const url = 'https://api.thecatapi.com/v1/images/search';
+const catUrl = 'https://api.thecatapi.com/v1/images/search';
+const dogUrl = 'https://api.thedogapi.com/v1/images/search';
 
-const fetchCatImg = async (url) => {
+const fetchImg = async (url) => {
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error('Error fetching advice');
@@ -15,28 +16,45 @@ const height = document.createElement('h2')
 height.id = 'height';
 
 
-const getCat = async () => {
-    const catContainer = document.querySelector('#cat-container')
+const getPet = async (petUrl) => {
+    const petContainer = document.querySelector('#pet-container')
     try {
-        const getCat = await fetchCatImg(url);
-        document.querySelector('#cat-image').src = getCat[0].url;
-        width.textContent = `Width: ${getCat[0].width}`;
-        height.textContent = `Height: ${getCat[0].height}`;
-        catContainer.appendChild(width)
-        catContainer.appendChild(height)
+        const getPet = await fetchImg(petUrl);
+        const petImg = document.querySelector('#pet-image')
+        petImg.src = getPet[0].url;
+        petImg.style.display = 'block'
+        width.textContent = `Width: ${getPet[0].width}`;
+        height.textContent = `Height: ${getPet[0].height}`;
+        petContainer.appendChild(width)
+        petContainer.appendChild(height)
+
 
     } catch (error) {
-        const errorMessage = document.createElement('h2')
-        errorMessage.textContent = 'Ups! The cat is hidden, please try again later.'
-        catContainer.appendChild(errorMessage);
+        const errorContainer = document.querySelector('#error-container');
+        errorContainer.innerHTML = '';
+        const errorMessage = document.createElement('h2');
+        errorMessage.textContent = 'Ups! The pet is hidden, please try again later.'
+        errorContainer.appendChild(errorMessage);
         console.error(error);
     }
 }
 
 window.addEventListener('load', () => {
-    getCat();
+    const pet = document.querySelector('#pet');
+    pet.addEventListener('change', () => {
+        if (pet.value === 'cat') {
+            getPet(catUrl);
+        } else if (pet.value === 'dog') {
+            getPet(dogUrl);
+        }
+    })
+    document.querySelector('#get-pet').addEventListener('click', () => {
+        if (pet.value === 'cat') {
+            getPet(catUrl);
+        } else if (pet.value === 'dog') {
+            getPet(dogUrl);
+        }
+    })
 })
 
-document.querySelector('#get-cat').addEventListener('click', () => {
-    getCat();
-})
+
